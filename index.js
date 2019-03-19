c0a482b5497ac7c0aa21c26b61dc3d0e42bc53d1
g s <gtsmang@gmail.com>
2019-03-18 22:43:28 -0700

    keyup functionality

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-$(function(){
-
-    $('form').submit(function(event){
-        event.preventDefault();
-        //$('h1').text("button");
-        let shopItem = $('#shopping-list-entry').val();
-        if(shopItem != "")
+function addItem()
+{
+    let shopItem = $('#shopping-list-entry').val();
+    if(shopItem != "")
         $('ul').append(`<li>
         <span class="shopping-item">${shopItem}</span>
         <div class="shopping-item-controls">
-          <button class="shopping-item-toggle">
+        <button class="shopping-item-toggle">
             <span class="button-label">check</span>
-          </button>
-          <button class="shopping-item-delete">
+        </button>
+        <button class="shopping-item-delete">
             <span class="button-label">delete</span>
-          </button>
+        </button>
         </div>
         </li>`)
+}
+
+$(function(){
+
+    $('#shopping-list-entry').keyup(function(event){
+        if(event.which()== 13)
+            addItem();
+    });
+
+    $('form').submit(function(event){
+        event.preventDefault();
+        addItem();
     });    
 
     $('ul').on('click', 'button', function(event){
         event.stopPropagation();
 
-        let pressedButton = $(this).find('.button-label').text();
+        let buttonText = $(this).find('.button-label').text();
 
-        if(pressedButton === "check")
-        {
+        if(buttonText === "check") {
             let shopitemObj = $(this).parent().prev();
-            //$(this).addClass("selected"); // change button
-
             shopitemObj.hasClass("shopping-item__checked") ?
             shopitemObj.removeClass("shopping-item__checked") :
             shopitemObj.addClass("shopping-item__checked");
-        }else if(pressedButton === "delete")
-        {
-            let listItem = $(this).parent().parent();
-            listItem.remove();
+        } else if(buttonText === "delete") {
+            $(this).parent().parent().remove();
         }         
     });
 })
